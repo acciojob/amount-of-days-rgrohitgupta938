0dd247cf92e3eff1fa30d452a3ec3fb2d0456856
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-02-09 01:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
+const input = parseInt(prompt("Enter year"));
+daysOfAYear(input);
